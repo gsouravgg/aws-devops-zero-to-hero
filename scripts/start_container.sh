65f8d63257214c0f47ce5ed4db2cb75d6cf043f9
gsouravgg <sourav2vasu@gmail.com>
2025-03-16 20:07:19 +0530

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-# Pull the Docker image from Docker Hub
+# Pull the Docker image from Do cker Hub
 docker pull gsouravgg/simple-python-flask-app
 # Run the Docker image as a container
 docker run -d -p 5000:5000 gsouravgg/simple-python-flask-app
